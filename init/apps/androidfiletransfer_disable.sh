47aa9276470470ff50c42db863fb41adf1b79449
Umi Uyura <umiuyura@gmail.com>
2015-03-10 23:16:20 +0900

    Homebrew Caskでのインストール先を変更していたので、スクリプトに反映

--- init/apps/androidfiletransfer_disable.sh
+++ init/apps/androidfiletransfer_disable.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mv "${HOME}/Applications/Android File Transfer.app/Contents/Resources/Android File Transfer Agent.app" "${HOME}/Applications/Android File Transfer.app/Contents/Resources/Android File Transfer Agent_DISABLED.app"
+mv "/Applications/Android File Transfer.app/Contents/Resources/Android File Transfer Agent.app" "/Applications/Android File Transfer.app/Contents/Resources/Android File Transfer Agent_DISABLED.app"
 mv "${HOME}/Library/Application Support/Google/Android File Transfer/Android File Transfer Agent.app" "${HOME}/Library/Application Support/Google/Android File Transfer/Android File Transfer Agent_DISABLED.app"
