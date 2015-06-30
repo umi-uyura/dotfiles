b582d08e9d98183e20bced63cdcf70a14fa9ae69
Umi Uyura <umiuyura@gmail.com>
2015-06-30 18:21:14 +0900

    Papertrailプラグイン追加

--- init/apps/heroku_plugins.sh
+++ init/apps/heroku_plugins.sh
@@ -3,4 +3,5 @@
 heroku plugins:install git://github.com/ddollar/heroku-accounts.git
 # heroku plugins:install git://github.com/hone/heroku_tab_complete_console.git
 # heroku plugins:install git://github.com/hone/heroku_switch_command.git
+heroku plugins:install heroku-papertrail
 heroku plugins:install git://github.com/hone/heroku-sendgrid-stats.git
