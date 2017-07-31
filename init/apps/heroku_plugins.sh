334f0434a51240bcbeea45807720c1a8fb24e1a4
Umi Uyura <umiuyura@gmail.com>
2017-07-31 23:30:13 +0900

    [Heroku] CLIプラグインを整理

--- init/apps/heroku_plugins.sh
+++ init/apps/heroku_plugins.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-heroku plugins:install git://github.com/ddollar/heroku-accounts.git
-# heroku plugins:install git://github.com/hone/heroku_tab_complete_console.git
-# heroku plugins:install git://github.com/hone/heroku_switch_command.git
-heroku plugins:install git://github.com/heroku/heroku-pg-extras.git
+heroku plugins:install heroku-accounts
+heroku plugins:install heroku-pg-extras
 heroku plugins:install heroku-papertrail
-heroku plugins:install git://github.com/hone/heroku-sendgrid-stats.git
