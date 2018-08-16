2174d4d9d10a69e499abe03deb1728a185f1bf53
Umi Uyura <umiuyura@gmail.com>
2018-08-16 10:03:10 +0900

    [Heroku] heroku-cli-autocompleteを追加

--- init/apps/heroku_plugins.sh
+++ init/apps/heroku_plugins.sh
@@ -3,3 +3,7 @@
 heroku plugins:install heroku-accounts
 heroku plugins:install heroku-pg-extras
 heroku plugins:install heroku-papertrail
+heroku plugins:install heroku-cli-autocomplete
+
+heroku autocomplete
+printf "$(heroku autocomplete:script bash)" >> ~/.bash_local; source ~/.bash_local
