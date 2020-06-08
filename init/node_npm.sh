92eb73dee90e682fc2b38eeb5982ac171a434778
Umi Uyura <umiuyura@gmail.com>
2020-06-08 18:00:56 +0900

    [npm] 最近の利用状況に合わせて整理

--- init/node_npm.sh
+++ init/node_npm.sh
@@ -7,59 +7,36 @@ npm update -g npm
 # Install npm packages
 #
 
-# Titanium
-npm install -g appcelerator
-npm install -g titanium
-npm install -g titanium-code-processor
-npm install -g alloy
-npm install -g acs
-npm install -g appc-npm
-npm install -g gittio
-npm install -g tn --unsafe-perm
-npm install -g git://github.com/mgostisha/alloy-smelter.git
-npm install -g ti-i18n
-npm install -g ticons
-npm install -g iconfont
-npm install -g faster-titanium
-npm install -g tishadow --unsafe-perm
-npm install -g dev.tiapp --unsafe-perm
-npm install -g tich
-npm install -g tisdk
-npm install -g ti-64
-npm install -g timodules
-npm install -g ti-deploygate-hook
-
-# Phonegap/Cordova
-# npm install -g phonegap
-npm install -g ios-sim
-npm install -g ios-deploy --unsafe-perm
-
-# Heroku
-npm install -g app.json
-
 # Web develop
-npm install -g node-static
+# npm install -g node-static
 npm install -g http-server
 npm install -g local-web-server
-npm install -g express-generator
-npm install -g jade
-npm install -g stylus
-npm install -g stylestats
-npm install -g supervisor
+# npm install -g express-generator
+# npm install -g pug
+# npm install -g stylus
+# npm install -g stylestats
+# npm install -g supervisor
 npm install -g easymock
 npm install -g psi
-npm install -g disc
+# npm install -g disc
 npm install -g tern
 npm install -g js-beautify
 
+# Vue.js
+npm install -g @vue/devtools
+npm install -g @vue/cli
+npm install -g @vue/cli-service-global
+npm install -g @gridsome/cli
+
 # Linter
 npm install -g eslint
+npm install -g eslint-plugin-vue
 npm install -g eslint-plugin-react
 npm install -g jsonlint-mod
 npm install -g plato
 
-# Development overall
-npm install -g ghcal
+# Heroku
+npm install -g app.json
 
 # Debug/Test
 npm install -g blink-diff
@@ -75,5 +52,3 @@ npm install -g unused-deps
 npm install -g lice
 npm install -g nlf
 npm install -g tldr
-npm install -g xcode-switch
-npm install -g speed-test
