9e53dfd6f665c0803ba127f4038b61fc93ea6521
Umi Uyura <umiuyura@gmail.com>
2020-06-08 18:02:14 +0900

    [Python] 最近の利用状況に合わせて整理

--- init/python_pip.sh
+++ init/python_pip.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
-pip install ipython
-pip install virtualenv
+# Update pip itself
+pip install --upgrade pip
+
+#
+# Install pip packages
+#
+
+# Linter
 pip install pylint
+
+# Utilities
+pip install python-dateutil
+
+# CLI
+pip install tqdm
+
+# Web develop
+pip install requests
+pip install beautifulsoup4
+pip install lxml
+pip install selenium
+
+# Scientific calculation
 pip install numpy
 pip install matplotlib
+
+# Network
+pip install speedtest-cli
+
+# Tools
 pip install cheat
 pip install wakatime
